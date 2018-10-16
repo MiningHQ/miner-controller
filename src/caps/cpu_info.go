9a66f5a0e2fd7636e8d7adab7caf7fe186217c7e
Donovan Solms <donovan.solms@gmail.com>
2018-10-16 11:23:06 +0200

    Updated CPU and Momery caps checking to be cross platform

--- src/caps/cpu_info.go
+++ src/caps/cpu_info.go
@@ -2,8 +2,9 @@ package caps
 
 import (
 	"fmt"
+	"strconv"
+	"strings"
 
-	"github.com/jaypipes/ghw"
 	"github.com/shirou/gopsutil/cpu"
 )
 
@@ -39,36 +40,70 @@ func (cpu CPUInfo) String() string {
 func GetCPUInfo() ([]CPUInfo, error) {
 	var cpuInfos []CPUInfo
 
-	cpuDetail, err := ghw.CPU()
-	if err != nil {
-		return cpuInfos, err
-	}
-	cpuAdditional, err := cpu.Info()
+	cpus, err := cpu.Info()
 	if err != nil {
 		return cpuInfos, err
 	}
 
-	// Get all the info we want for each CPU installed. ghw does not provide
-	// cache size, gopsutil does, we loop over all the CPUs to match
-	// the IDs and get the additional information
-	for _, processor := range cpuDetail.Processors {
-		var processorExtended cpu.InfoStat
-		for _, processorAdditional := range cpuAdditional {
-			if uint32(processorAdditional.CPU) == processor.Id {
-				processorExtended = processorAdditional
-			}
+	// Get all the info we want for each CPU installed
+	// gopsutil provides the CPU information per CPU thread/logical CPU. Thus
+	// we need to loop and determine the actual values of each thread's base
+	// processorThread.Cores does not return the total value we need
+	var currentCPU CPUInfo
+	currentCPUID := 0
+	for _, processorThread := range cpus {
+
+		cpuID, err := strconv.Atoi(processorThread.PhysicalID)
+		if err != nil {
+			return cpuInfos, fmt.Errorf("Unable to get CPU ID: %s", err)
 		}
-		cpuInfos = append(cpuInfos, CPUInfo{
-			ID:             processor.Id,
-			Vendor:         processor.Vendor,
-			Product:        processor.Model,
-			GHz:            float64(processorExtended.Mhz / 1000.00),
-			Cores:          processor.NumCores,
-			Threads:        processor.NumThreads,
-			CacheMB:        uint32(processorExtended.CacheSize / 1024),
-			HasHardwareAES: processor.HasCapability("aes"),
-		})
+
+		// When we get to a new physical CPU, add the currentCPU
+		if currentCPUID != cpuID {
+			currentCPU.fixCounts()
+			cpuInfos = append(cpuInfos, currentCPU)
+
+			currentCPU = CPUInfo{}
+			currentCPUID = cpuID
+		}
+
+		currentCPU.ID = uint32(cpuID)
+		currentCPU.CacheMB = uint32(processorThread.CacheSize / 1024)
+		currentCPU.Vendor = processorThread.VendorID
+		currentCPU.Product = processorThread.ModelName
+		currentCPU.GHz = processorThread.Mhz / 1000
+		currentCPU.HasHardwareAES = hasAES(processorThread)
+		currentCPU.Threads++
+
+		// coreID is a sequential core list that we can use to get the correct
+		// core count for the CPU
+		coreID, err := strconv.Atoi(processorThread.CoreID)
+		if err != nil {
+			return cpuInfos, fmt.Errorf("Unable to get CPU Core ID: %s", err)
+		}
+		if uint32(coreID) > currentCPU.Cores {
+			currentCPU.Cores = uint32(coreID)
+		}
+
 	}
+	// Add last one as well
+	currentCPU.fixCounts()
+	cpuInfos = append(cpuInfos, currentCPU)
 
 	return cpuInfos, nil
+}
+
+// fixCounts increases cores by one because the IDs start at zero
+func (cpu *CPUInfo) fixCounts() {
+	cpu.Cores++
+}
+
+// hasAES checks if the given CPU Info contains the AES flag
+func hasAES(info cpu.InfoStat) bool {
+	for _, flag := range info.Flags {
+		if strings.Contains(strings.ToLower(flag), "aes") {
+			return true
+		}
+	}
+	return false
 }
--- src/caps/memory_info.go
+++ src/caps/memory_info.go
@@ -3,32 +3,33 @@ package caps
 import (
 	"fmt"
 
-	"github.com/jaypipes/ghw"
+	"github.com/shirou/gopsutil/mem"
 )
 
 // MemoryInfo contains the information about installed memory
 type MemoryInfo struct {
-	PhysicalMB int64
-	UsableMB   int64
+	TotalMB     uint64
+	AvailableMB uint64
 }
 
 // String converts MemoryInfo into a readable string
 func (memory MemoryInfo) String() string {
 	return fmt.Sprintf(
-		"Memory: Physical %d MB, Usable %d MB",
-		memory.PhysicalMB,
-		memory.UsableMB)
+		"Memory: Total %d MB, Available %d MB",
+		memory.TotalMB,
+		memory.AvailableMB)
 }
 
 // GetMemoryInfo returns the installed memory in the system
 func GetMemoryInfo() (MemoryInfo, error) {
-	memory, err := ghw.Memory()
+	memory, err := mem.VirtualMemory()
 	if err != nil {
 		return MemoryInfo{}, err
 	}
+
 	// Return the values in megabyte
 	return MemoryInfo{
-		PhysicalMB: memory.TotalPhysicalBytes / 1024 / 1024,
-		UsableMB:   memory.TotalUsableBytes / 1024 / 1024,
+		TotalMB:     memory.Total / 1024 / 1024,
+		AvailableMB: memory.Available / 1024 / 1024,
 	}, nil
 }
