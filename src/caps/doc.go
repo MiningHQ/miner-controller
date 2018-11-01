b5356e94447a37ea2e69bc37c70b0d47e82576d8
Donovan Solms <donovan.solms@gmail.com>
2018-11-01 21:48:23 +0200

    Moved caps to spec package

--- src/caps/doc.go
+++
@@ -1,8 +0,0 @@
-// Package caps provides utilities to check the capabilities of the current
-// system important to cryptocurrency mining.
-//
-// Capabilities that can be checked:
-//  1. CPUs - core count, threads and L1, L2 and L3 caches
-//  2. Memory
-//  3. GPUs - Vendor, OpenCL/CUDA support and memory
-package caps
